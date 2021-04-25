68bfa5a990ae7980e521fa98211eeed220dd6556
Arjun Krishna <arjun12345krishna@gmail.com>
2021-04-26 03:16:00 +0530

    Basic File Upload - Complete, Success Upload Page - Complete

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var upload = require('express-fileupload');
 var path = require('path');
 var hbs = require('express-handlebars');
 
 var app = express();
+app.use(upload());
 
 app.set('views', (__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -22,5 +24,19 @@ app.get('/', (req, res) => {
 app.get('/upload', (req, res) => {
     res.render('file-upload');
 });
+
+app.post('/upload', (req, res) => {
+    if(req.files) {
+        // console.log(req.files);
+
+        var file = req.files.file;
+        // console.log(file.name);
+
+        file.mv('./uploads/' + file.name, (err) => {
+            // res.send('File Uploaded')
+            res.render('upload-success');
+        })
+    }
+})
 
 app.listen(3000);
